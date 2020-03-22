85badd1c28f5873e4729fbfa4a6acef0ac2a5009
hk9696 <2368727286@qq.com>
2020-03-22 17:15:13 +0800

    itli

---
+++ src/sample/Hello.java
@@ -0,0 +1,26 @@
+package sample;
+
+import com.sun.corba.se.impl.encoding.OSFCodeSetRegistry;
+import com.sun.xml.internal.ws.api.server.SDDocumentFilter;
+import sun.security.provider.PolicySpiFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author hukun
+ * @create 2020-03-22 11:48
+ */
+public class Hello {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        for (int i = 0; i < list.size(); i++) {
+            Object o =  list.get(i);
+            
+        }
+
+
+    }
+}
